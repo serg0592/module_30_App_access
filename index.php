aa623c425481be56c0967ca08eaa011c8498568f
Sergey Kudelin <kudelin.work@bk.ru>
2022-05-31 22:33:58 +0300

    Studying(2)

--- index.php
+++ index.php
@@ -1,29 +1,49 @@
-<?php
-    echo "hi";
-    var_dump($_GET);
+<?php 
+  include 'config/lib.php';
+  $pages = include 'config/pages.php';
+  var_dump($pages);
+  $page = getPage($pages);
+  //$page = (int)$_GET['page']; 
 ?>
-
-<form method="POST">
-  <p>Ваш логин: 
-    <input type="text" name="login" />
-  </p>
-  <p>Ваш пароль: 
-    <input type="text" name="password" />
-  </p>
-  <input type="checkbox" name="test[]" value="1">
-  <input type="checkbox" name="test[]" value="2">
-  <input type="checkbox" name="test[]" value="3">
-  <input type="submit"  value="отправить">
-</form>
-
-<ul>
-  <li>
-    <a href="?page=1">Главная</a>
-  </li>
-  <li>
-    <a href="?page=2">Пользователи</a>
-  </li>
-  <li>
-    <a href="?page=3">Пользователь</a>
-  </li>
-</ul>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header></header>
+        <main>
+        <?php
+            echo "hi";
+            var_dump($_GET);
+        ?>
+        <form method="POST">
+          <p>Ваш логин: 
+            <input type="text" name="login" />
+          </p>
+          <p>Ваш пароль: 
+            <input type="text" name="password" />
+          </p>
+          <input type="checkbox" name="test[]" value="1">
+          <input type="checkbox" name="test[]" value="2">
+          <input type="checkbox" name="test[]" value="3">
+          <input type="submit"  value="отправить">
+        </form>
+        <ul>
+          <li>
+            <a href="?page=1">Главная</a>
+          </li>
+          <li>
+            <a href="?page=2">Пользователи</a>
+          </li>
+          <li>
+            <a href="?page=3">Пользователь</a>
+          </li>
+        </ul>
+        </main>
+        <footer></footer>
+        <?php
+          include 'pages/' . $page;
+        ?>
+    </body>
+</html>
