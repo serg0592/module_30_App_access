dbce01a773ae50a467c3e6548cf1d1e488b8b3b6
Sergey Kudelin <kudelin.work@bk.ru>
2022-05-31 16:14:11 +0300

    Studying

---
+++ config/lib.php
@@ -0,0 +1,15 @@
+<?php 
+    function getPage(array $pages) { 
+        $pageNumber = 1; 
+
+        if (!empty($_GET['page'])) { 
+            $pageNumber = (int) $_GET['page']; 
+        } 
+
+        if (empty($pages[$pageNumber])) { 
+            $pageNumber = 1; 
+        } 
+        
+        return  $pages[$pageNumber]; 
+    } 
+?>
