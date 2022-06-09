d6edd068c42789ab278fcceb2d4b239433194e47
Sergey Kudelin <kudelin.work@bk.ru>
2022-06-09 10:20:19 +0300

    Fixed: 30.4

--- index.php
+++ index.php
@@ -8,6 +8,32 @@
         if (!empty($_POST['login']) && !empty($_POST['password'])) {
             $_SESSION['login'] = $_POST['login'];
             $_SESSION['password'] = $_POST['password'];
+
+            $log = $_SESSION['login'];
+            $pas = $_SESSION['password'];
+            $check_log = [];
+            $check_log_pas = [];
+
+            $host = 'localhost';
+            $user = 'root';
+            $password = '';
+            $db_name = '30.4_practice';
+
+            $link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
+            mysqli_query($link, "SET NAMES 'utf8'");    
+            $query_log = mysqli_query($link, "SELECT * FROM users WHERE user_login = '$log'");
+
+            if (!empty($check_log[] = mysqli_fetch_assoc($query_log))) {
+                $query_pas = mysqli_query($link, "SELECT * FROM users WHERE user_login = '$log' AND user_password = '$pas'");
+                if (!empty($check_log_pas[] = mysqli_fetch_assoc($query_pas))) {
+                    echo 'авторизован';
+                } else {
+                    echo 'неверный пароль';
+                }
+            } else {
+                echo 'такого пользователя не существует (неверный логин)';
+            }
+            
         } else {
             echo 'введите логин и пароль';
         }
@@ -22,7 +48,6 @@
     </head>
     <body>
         <form method="POST" action="">
-          Авторизация<br>
           Логин<br>
           <input type="text" name="login"/><br>
           Пароль<br>
